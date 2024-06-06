c1cfa6a3b9e32cb2404657c2a6c0744fd00c9572
akemicomette <akemicomette@gmail.com>
2024-06-06 20:04:51 -0300

    refactor: exercise 26 and 27

--- MODULO01/atividade02/exercicio27.js
+++ MODULO01/atividade02/exercicio27.js
@@ -1,36 +1,26 @@
 const prompt = require("prompt-sync")();
 const { verifyValidEntrance } = require("./verficaEntradaValida.js");
+const { generateMatrix } = require("./generateMatrix.js");
 
 function exercicio27(){
   
-  let count = 1;
-  let matrixM = [];
+  const matrixM = generateMatrix(6,6);
   
-  for(let row = 0; row < 2; row++) {
-    const line = [];
-    for(let column = 0; column < 2; column++) {
-      let number = parseFloat(prompt(`Insira o ${count} número da matriz M: `));
-      if(verifyValidEntrance(number) === false) {
-        return;
-      }
-      line.push(number);
-      count++;
-    }
-    matrixM.push(line);
-  }
-
   let vector = [];
   let multiplier = parseInt(prompt('Insira o multiplicador: '));
-  for(let row = 0; row < 2; row++){
-    for(let cell = 0; cell < 2; cell++){
+  if(verifyValidEntrance(multiplier) === false){
+    return;
+  }
+
+  for(let row = 0; row < 6; row++){
+    for(let cell = 0; cell < 6; cell++){
       let result = matrixM[row][cell] * multiplier;
       vector.push(result);
     }
   }
  
-  console.log('vetor é: ', vector);
   console.log('matriz M: ', matrixM);
-    
+  console.log('vetor é: ', vector);
 }
   
  exercicio27();
