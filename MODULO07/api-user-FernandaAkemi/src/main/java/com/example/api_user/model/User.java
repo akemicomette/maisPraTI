b2e87c32be86d25a3eeb3958678cc4f5489f2e87
akemicomette <akemicomette@gmail.com>
2024-10-20 21:20:11 -0300

    feat: exercise 13 module 07

---
+++ MODULO07/api-user-FernandaAkemi/src/main/java/com/example/api_user/model/User.java
@@ -0,0 +1,30 @@
+package com.example.api_user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String role;
+
+    public String getUsername() {
+        return null;
+    }
+
+    public String getPassword() {
+        return null;
+    }
+}
